f8358d8eec20615d2e17d7cd9334860e6440ac4a
mahmut ozgurdere <ozgurdere@gmail.com>
2025-11-23 15:38:35 +0000

    starting build "2d7ad87a-706f-4bc3-a290-7ca8b0a514f4" FETCHSOURCE BUILD

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This is to allow the Next.js dev server to accept requests from the
-    // Firebase Studio development environment.
-    allowedDevOrigins: ['*'],
-  },
 };
 
 export default nextConfig;
