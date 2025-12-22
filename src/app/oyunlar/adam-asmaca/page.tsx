4f555c531e7b18c549adb1a224848d8c324101fe
mahmut ozgurdere <ozgurdere@gmail.com>
2025-12-22 18:31:06 +0000

    bil bakalımda geliyor ama adam asmaca da gelmiyor sınıflar ne iş

--- src/app/oyunlar/adam-asmaca/page.tsx
+++ src/app/oyunlar/adam-asmaca/page.tsx
@@ -6,14 +6,13 @@ import { OyunKurulum } from '@/components/oyun-kurulum';
 import { Skull, Loader2 } from 'lucide-react';
 
 function AdamAsmacaPage() {
-    const isStatic = process.env.NEXT_PUBLIC_STATIC_BUILD === 'true';
     return (
         <OyunKurulum 
             gameName="Adam Asmaca"
             gameIcon={Skull}
             gamePath="adam-asmaca"
             dataType="games"
-            isStatic={isStatic}
+            isStatic={true}
         />
     );
 }
