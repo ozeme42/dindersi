8e68d979eb22314a7892aa00a80c5754258dbf40
mahmut ozgurdere <ozgurdere@gmail.com>
2025-12-24 05:10:07 +0000

    Try fixing this error: `Runtime Error: The default export is not a React

--- src/app/oyunlar/kavram-yarismasi/oyun/page.tsx
+++
@@ -1,4 +0,0 @@
-// Bu dosyanın içeriği silindi ve mantığı
-// /oyunlar/kavram-yarismasi/oyun/konu/[topicId]/page.tsx dosyasına taşındı.
-// Bu, dinamik yönlendirmeyi basitleştirmek ve URL'den ID okuma
-// hatalarını önlemek için yapıldı.
